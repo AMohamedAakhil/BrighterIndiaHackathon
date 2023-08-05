b1dcb0fc4d847eaed83465a105a14b207b24dbdb
AMohamedAakhil <a.aakhilmohamed@gmail.com>
2023-08-05 18:02:55 +0530

    add server

--- model/main.py
+++ model/main.py
@@ -2,27 +2,23 @@ import moviepy.editor as mp
 import requests
 import openai
 import os
-openai.api_key = "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"
 from pytube import YouTube
 
 def get_sub(link):
     def download_360p_mp4_videos(url: str, outpath: str = "./", custom_filename: str = "my_video.mp4"):
         yt = YouTube(url)
         yt.streams.filter(file_extension="mp4").get_by_resolution("360p").download(output_path=outpath, filename=custom_filename)
-
-
-    if __name__ == "__main__":
-        download_360p_mp4_videos(
+    
+    download_360p_mp4_videos(
             link,
             "./downloads",
-            "vid.mp4"
-        )
+            "vid.mp4")
 
 
     filename = "./downloads/vid.mp4" 
 
     video = mp.VideoFileClip(filename)
-    openai.api_key = "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"
+    openai.api_key = "OPENAI"
     
     result = openai.Audio.transcribe(
         model='whisper-1',
@@ -53,7 +49,7 @@ def get_sub(link):
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-            {"role": "system", "content": "You will be given a subtitle file, in SRT format. You have to parse through the text in the file and return only the important parts in the same SRT format. You have to select only the most important 2 minutes of the clip."},
+            {"role": "system", "content": "You will be given a subtitle file, in SRT format. First part is not important. Get excerpts from the middle of the file . You have to select only the most impactful 1 minute of the text."},
             {"role": "user", "content": subtitles},
         ]
     )
